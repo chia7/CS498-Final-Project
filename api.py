b745a0eac92bab9d2727fd3c0dbfbf0e09a8dda5
chia7 <b04902097@ntu.edu.tw>
2022-04-26 19:42:56 -0500

    Complete static webpage for Q1-Q4 and Q6

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 
 import redis
@@ -28,11 +28,16 @@ def exec(r, query, name='Twitter'):
 
 r = redis.StrictRedis(host='localhost', port=6379, decode_responses=True)
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='static')
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/')
 def index():
+    return send_from_directory(app.static_folder, "index.html")
+
+
+@app.route('/health')
+def health():
     return jsonify(success=True)
 
 
@@ -47,11 +52,30 @@ def question1():
         return jsonify(error="parameter `screen_name` not found")
 
     query = """MATCH (a:User {screen_name: $screen_name})-[:Post]->(t_a:Tweet {type:'tweet'})<-[:Reply]-(t_b:Tweet {type:'reply'})<-[:Post]-(b:User) \
-               RETURN a.screen_name, t_a.text, t_a.timestamp, COLLECT([b.screen_name, t_b.text, t_b.timestamp]) \
+               RETURN a.id, a.screen_name, t_a.id, t_a.text, t_a.timestamp, b.id, b.screen_name, t_b.id, t_b.text, t_b.timestamp \
                ORDER BY t_a.timestamp DESC, t_b.timestamp DESC"""
 
     query = build_params_header(params) + query
-    resp = exec(r, query)
+
+    results = exec(r, query)[1]
+
+    resp = []
+    for result in results:
+        a_id = result[0][1]
+        a_screen_name = result[1][1]
+        ta_id = result[2][1]
+        ta_text = result[3][1]
+        ta_ts = result[4][1]
+        b_id = result[5][1]
+        b_screen_name = result[6][1]
+        tb_id = result[7][1]
+        tb_text = result[8][1]
+        tb_ts = result[9][1]
+
+        if len(resp) > 0 and resp[-1][2] == ta_id:
+            resp[-1][5].append([b_id, b_screen_name, tb_id, tb_text, tb_ts])
+        else:
+            resp.append([a_id, a_screen_name, ta_id, ta_text, ta_ts, [[b_id, b_screen_name, tb_id, tb_text, tb_ts]]])
 
     return jsonify(resp)
 
@@ -59,11 +83,32 @@ def question1():
 @app.route('/q2', methods=['GET'])
 def question2():
 
-    query = """MATCH (c:Country)<-[:At]-(:Tweet) \
-               RETURN c.text, count(*) AS CNT \
-               ORDER BY CNT DESC"""
+    params = {
+        'start_ts': int(request.args.get('start_ts')) if request.args.get('start_ts') is not None else None,
+        'end_ts': int(request.args.get('end_ts')) if request.args.get('end_ts') is not None else None,
+    }
 
-    resp = exec(r, query)
+    query = """MATCH (c:Country)<-[:At]-(t:Tweet) """
+
+    if params['start_ts'] is not None and params['end_ts'] is not None:
+        query += """WHERE t.timestamp >= $start_ts AND t.timestamp <= $end_ts """
+    elif params['start_ts'] is not None:
+        query += """WHERE t.timestamp >= $start_ts """
+    elif params['end_ts'] is not None:
+        query += """WHERE t.timestamp <= $end_ts """
+
+    query += """RETURN c.text, count(*) AS CNT \
+                ORDER BY CNT DESC LIMIT 8"""
+
+    query = build_params_header(params) + query
+
+    results = exec(r, query)[1]
+
+    resp = []
+    for result in results:
+        country = result[0][1]
+        cnt = result[1][1]
+        resp.append([country, cnt])
 
     return jsonify(resp)
 
@@ -71,11 +116,33 @@ def question2():
 @app.route('/q3', methods=['GET'])
 def question3():
 
-    query = """MATCH (u:User)-[:Post]->(:Tweet {type:'tweet'}) \
-               RETURN u.id, u.screen_name, count(*) AS CNT \
-               ORDER BY CNT DESC LIMIT 10"""
+    params = {
+        'start_ts': int(request.args.get('start_ts')) if request.args.get('start_ts') is not None else None,
+        'end_ts': int(request.args.get('end_ts')) if request.args.get('end_ts') is not None else None,
+    }
 
-    resp = exec(r, query)
+    query = """MATCH (u:User)-[:Post]->(t:Tweet {type:'tweet'}) """
+
+    if params['start_ts'] is not None and params['end_ts'] is not None:
+        query += """WHERE t.timestamp >= $start_ts AND t.timestamp <= $end_ts """
+    elif params['start_ts'] is not None:
+        query += """WHERE t.timestamp >= $start_ts """
+    elif params['end_ts'] is not None:
+        query += """WHERE t.timestamp <= $end_ts """
+
+    query += """RETURN u.id, u.screen_name, count(*) AS CNT \
+                ORDER BY CNT DESC LIMIT 10"""
+
+    query = build_params_header(params) + query
+
+    results = exec(r, query)[1]
+
+    resp = []
+    for result in results:
+        uid = result[0][1]
+        screen_name = result[1][1]
+        cnt = result[2][1]
+        resp.append([uid, screen_name, cnt])
 
     return jsonify(resp)
 
@@ -83,9 +150,35 @@ def question3():
 @app.route('/q4', methods=['GET'])
 def question4():
 
-    query = """MATCH (h:Hashtag)<-[:Tag]-(t:Tweet) \
-               RETURN h.text, count(*) AS CNT, COLLECT(t.timestamp) \
-               ORDER BY CNT DESC LIMIT 100"""
+    #   86400 seconds = 1 day
+    #  604800 seconds = 1 week
+    # 2629743 seconds = 1 month
+    aggregation = 2629743
+
+    if request.args.get('aggregation') is not None:
+        if request.args.get('aggregation') == "day":
+            aggregation = 86400
+        elif request.args.get('aggregation') == "week":
+            aggregation = 604800
+
+    params = {
+        'start_ts': int(request.args.get('start_ts')) if request.args.get('start_ts') is not None else None,
+        'end_ts': int(request.args.get('end_ts')) if request.args.get('end_ts') is not None else None,
+    }
+
+    query = """MATCH (h:Hashtag)<-[:Tag]-(t:Tweet) """
+
+    if params['start_ts'] is not None and params['end_ts'] is not None:
+        query += """WHERE t.timestamp >= $start_ts AND t.timestamp <= $end_ts """
+    elif params['start_ts'] is not None:
+        query += """WHERE t.timestamp >= $start_ts """
+    elif params['end_ts'] is not None:
+        query += """WHERE t.timestamp <= $end_ts """
+
+    query += """RETURN h.text, count(*) AS CNT, COLLECT(t.timestamp) \
+                ORDER BY CNT DESC LIMIT 100"""
+
+    query = build_params_header(params) + query
 
     results = exec(r, query)[1]
 
@@ -95,7 +188,7 @@ def question4():
         count = result[1][1]
         timestamps = {}
         for t in result[2][1]:
-            ts = ( t[1] // 2629743 ) * 2629743  # 2629743 seconds = 1 month
+            ts = ( t[1] // aggregation ) * aggregation
             timestamps[ts] = timestamps.get(ts, 0) + 1
         timestamps = [[k, v] for k, v in timestamps.items()]
         timestamps.sort()
@@ -145,10 +238,25 @@ def question6():
         screen_name = result[1][1]
         _type = result[2][1]
         count = result[3][1]
-        if len(resp) > 0 and resp[-1][0] == _id:
-            resp[-1].append([_type, count])
-        else:
-            resp.append([_id, screen_name, [_type, count]])
+
+        if len(resp) == 0:
+            resp.append([_id, screen_name, [0, 0, 0, 0]])
+
+        if resp[-1][0] != _id:
+            total = sum(resp[-1][2])
+            for i in range(4):
+                resp[-1][2][i] = (resp[-1][2][i] / total) * 100
+                resp[-1][2][i] = "{:.2f}".format(resp[-1][2][i])
+            resp.append([_id, screen_name, [0, 0, 0, 0]])
+
+        if _type == "tweet":
+            resp[-1][2][0] = count
+        elif _type == "reply":
+            resp[-1][2][1] = count
+        elif _type == "retweet":
+            resp[-1][2][2] = count
+        elif _type == "quotedRetweet":
+            resp[-1][2][3] = count
 
     return jsonify(resp)
 
---
+++ static/js/api.js
@@ -0,0 +1,346 @@
+function q1() {
+  var table = document.getElementById("q1Table");
+  table.style.display = "none";
+  table.innerHTML = "";
+
+  var screen_name = document.getElementById("q1_screen_name").value;
+  var url = "/q1?screen_name=" + screen_name;
+  
+  fetch(url).then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    table.style.display = "table";
+
+    data.forEach(function (item, index) {
+      var row = table.insertRow(-1);
+      var c1 = row.insertCell(0);
+      var c2 = row.insertCell(1);
+      var c3 = row.insertCell(2);
+      var c4 = row.insertCell(3);
+      var d = new Date(item[4] * 1000);
+
+      row.style.backgroundColor = "#dddddd";
+      c1.innerHTML = item[1];
+      c2.innerHTML = item[2];
+      c3.innerHTML = item[3];
+      c4.innerHTML = d.toLocaleTimeString('en-US') + ' ' + d.toLocaleDateString();
+
+      item[5].forEach(function (reply, idx) {
+        var r = table.insertRow(-1);
+        var r1 = r.insertCell(0);
+        var r2 = r.insertCell(1);
+        var r3 = r.insertCell(2);
+        var r4 = r.insertCell(3);
+        var rd = new Date(reply[4] * 1000);
+
+        r.style.backgroundColor = "#FFFFFF";
+        r.style.borderStyle = "hidden";
+        r1.style.borderStyle = "hidden";
+        r1.style.paddingLeft = "30px";
+        r2.style.borderStyle = "hidden";
+        r3.style.borderStyle = "hidden";
+        r4.style.borderStyle = "hidden";
+
+        r1.innerHTML = reply[1];
+        r2.innerHTML = reply[2];
+        r3.innerHTML = reply[3];
+        r4.innerHTML = rd.toLocaleTimeString('en-US') + ' ' + rd.toLocaleDateString();
+      });
+
+      row = table.insertRow(-1);
+      row.style.backgroundColor = "#FFFFFF";
+      row.style.borderStyle = "hidden";
+      c1 = row.insertCell(0);
+      c2 = row.insertCell(1);
+      c3 = row.insertCell(2);
+      c4 = row.insertCell(2);
+      c1.style.borderStyle = "hidden";
+      c2.style.borderStyle = "hidden";
+      c3.style.borderStyle = "hidden";
+      c4.style.borderStyle = "hidden";
+    });
+
+  }).catch(function() {
+    console.log("API failed");
+  });
+}
+
+function q2() {
+  var table = document.getElementById("q2Table");
+  table.style.display = "none";
+  table.innerHTML = "<tr><th>Rank</th><th>Country</th><th>Count</th></tr>";
+
+  var chart = new CanvasJS.Chart("q2Chart", {
+    animationEnabled: true,
+    theme: "light2", // "light1", "light2", "dark1", "dark2"
+    title:{
+      text:"Top 8 Active Countries"
+    },
+    axisY:{
+      title: "Number of Tweets",
+    },
+    data: [],
+  });
+
+  var url = "/q2";
+
+  var params = [];
+  if (document.getElementById("q2_start_date").value.length > 0) {
+    var t = new Date(document.getElementById("q2_start_date").value).getTime() / 1000;
+    params.push(['start_ts', t]);
+  }
+  if (document.getElementById("q2_end_date").value.length > 0) {
+    var t = new Date(document.getElementById("q2_end_date").value).getTime() / 1000;
+    params.push(['end_ts', t]);
+  }
+  params.forEach(function (param, index) {
+    if (index == 0) {
+      url += '?'
+    } else {
+      url += '&&'
+    }
+    url += param[0] + '=' + param[1];
+  });
+  
+  fetch(url).then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    document.getElementById("q2Chart").style.display = "block";
+    table.style.display = "table";
+
+    dps = [];
+    data.forEach(function (item, index) {
+      var row = table.insertRow(-1);
+      var c1 = row.insertCell(0);
+      var c2 = row.insertCell(1);
+      var c3 = row.insertCell(2);
+      c1.innerHTML = index + 1;
+      c2.innerHTML = item[0];
+      c3.innerHTML = item[1];
+
+      dps.push({y: item[1], label: item[0]});
+    });
+
+    chart.options.data.push({
+      type:"column",
+      dataPoints: dps,
+    });
+
+    chart.render();
+
+  }).catch(function() {
+    console.log("API failed");
+  });
+}
+
+function q3() {
+  var table = document.getElementById("q3Table");
+  table.style.display = "none";
+  table.innerHTML = "<tr><th>Rank</th><th>UID</th><th>Screen Name</th><th>Count</th></tr>";
+
+  var chart = new CanvasJS.Chart("q3Chart", {
+    animationEnabled: true,
+    
+    title:{
+      text:"Top 10 Active Users"
+    },
+    axisX:{
+      interval: 1
+    },
+    axisY:{
+      interlacedColor: "rgba(1,77,101,.2)",
+      gridColor: "rgba(1,77,101,.1)",
+      title: "Number of Tweets",
+    },
+    data: [],
+  });
+
+  var url = "/q3";
+
+  var params = [];
+  if (document.getElementById("q3_start_date").value.length > 0) {
+    var t = new Date(document.getElementById("q3_start_date").value).getTime() / 1000;
+    params.push(['start_ts', t]);
+  }
+  if (document.getElementById("q3_end_date").value.length > 0) {
+    var t = new Date(document.getElementById("q3_end_date").value).getTime() / 1000;
+    params.push(['end_ts', t]);
+  }
+  params.forEach(function (param, index) {
+    if (index == 0) {
+      url += '?'
+    } else {
+      url += '&&'
+    }
+    url += param[0] + '=' + param[1];
+  });
+  
+  fetch(url).then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    document.getElementById("q3Chart").style.display = "block";
+    table.style.display = "table";
+
+    dps = [];
+    data.forEach(function (item, index) {
+      var row = table.insertRow(-1);
+      var c1 = row.insertCell(0);
+      var c2 = row.insertCell(1);
+      var c3 = row.insertCell(2);
+      var c4 = row.insertCell(3);
+      c1.innerHTML = index + 1;
+      c2.innerHTML = item[0];
+      c3.innerHTML = item[1];
+      c4.innerHTML = item[2];
+
+      dps.unshift({y: item[2], label: item[1]});
+    });
+
+    chart.options.data.push({
+      type:"bar",
+      color: "#014D65",
+      dataPoints: dps,
+    });
+
+    chart.render();
+
+  }).catch(function() {
+    console.log("API failed");
+  });
+}
+
+function q4() {
+  var table = document.getElementById("q4Table");
+  table.style.display = "none";
+  table.innerHTML = "<tr><th>Rank</th><th>Hashtag</th><th>Count</th></tr>";
+
+  var chart = new CanvasJS.Chart("q4Chart", {
+    animationEnabled: true,
+    zoomEnabled: true,
+    title: {
+      text: "Hashtag Trends"
+    },
+    axisX: {
+      valueFormatString: "DD MMM YYYY"
+    },
+    axisY2: {
+      title: "Count",
+    },
+    toolTip: {
+      shared: true
+    },
+    legend: {
+      cursor: "pointer",
+      verticalAlign: "top",
+      horizontalAlign: "center",
+      dockInsidePlotArea: true,
+      itemclick: toogleDataSeries
+    },
+    data: [],
+  });
+
+  function toogleDataSeries(e) {
+    if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+      e.dataSeries.visible = false;
+    } else {
+      e.dataSeries.visible = true;
+    }
+    chart.render();
+  }
+
+  var url = "/q4";
+
+  var params = [];
+  if (document.getElementById("q4_start_date").value.length > 0) {
+    var t = new Date(document.getElementById("q4_start_date").value).getTime() / 1000;
+    params.push(['start_ts', t]);
+  }
+  if (document.getElementById("q4_end_date").value.length > 0) {
+    var t = new Date(document.getElementById("q4_end_date").value).getTime() / 1000;
+    params.push(['end_ts', t]);
+  }
+  if (document.getElementById("q4_aggregation").value.length > 0) {
+    params.push(['aggregation', document.getElementById("q4_aggregation").value]);
+  }
+  params.forEach(function (param, index) {
+    if (index == 0) {
+      url += '?'
+    } else {
+      url += '&&'
+    }
+    url += param[0] + '=' + param[1];
+  });
+  
+  fetch(url).then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    document.getElementById("q4Chart").style.display = "block";
+    table.style.display = "table";
+
+    data.forEach(function (item, index) {
+      var row = table.insertRow(-1);
+      var c1 = row.insertCell(0);
+      var c2 = row.insertCell(1);
+      var c3 = row.insertCell(2);
+      c1.innerHTML = index + 1;
+      c2.innerHTML = item[0];
+      c3.innerHTML = item[1];
+
+      if (index < 10) {
+        dps = [];
+        item[2].forEach(function (ts, idx) {
+          dps.push({x: new Date(ts[0] * 1000), y: ts[1]})
+        });
+
+        chart.options.data.push({
+          type:"line",
+          axisYType: "secondary",
+          name: item[0],
+          showInLegend: true,
+          markerSize: 1,
+          yValueFormatString: "#,###",
+          dataPoints: dps,
+        });
+      }
+    });
+
+    chart.render();
+
+  }).catch(function() {
+    console.log("API failed");
+  });
+}
+
+function q6() {
+  var table = document.getElementById("q6Table");
+  table.style.display = "none";
+  table.innerHTML = "<tr><th>Screen Name</th><th>UID</th><th>Simple Tweet</th><th>Reply</th><th>Retweet</th><th>Quoted Retweet</th></tr>";
+
+  var url = "/q6"
+  
+  fetch(url).then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    table.style.display = "table";
+
+    data.forEach(function (item, index) {
+      var row = table.insertRow(-1);
+      var c1 = row.insertCell(0);
+      var c2 = row.insertCell(1);
+      var c3 = row.insertCell(2);
+      var c4 = row.insertCell(3);
+      var c5 = row.insertCell(4);
+      var c6 = row.insertCell(5);
+
+      c1.innerHTML = item[1];
+      c2.innerHTML = item[0];
+      c3.innerHTML = item[2][0] + "%";
+      c4.innerHTML = item[2][1] + "%";
+      c5.innerHTML = item[2][2] + "%";
+      c6.innerHTML = item[2][3] + "%";
+    });
+
+  }).catch(function() {
+    console.log("API failed");
+  });
+}
